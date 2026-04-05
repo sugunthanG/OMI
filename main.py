453269ae90e6b745704323bb6ed2e0f377b49f5e
sugunthanG <sugunthan100@gmail.com>
2026-04-05 15:41:31 +0530

    debug: test streamlit render

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# import streamlit as st
+# from dashboard.dashboard import run_dashboard
+
+# st.session_state.logged_in = True
+
+# if __name__ == "__main__":
+#     run_dashboard()
+
 import streamlit as st
-from dashboard.dashboard import run_dashboard
 
-st.session_state.logged_in = True
-
-if __name__ == "__main__":
-    run_dashboard()
+st.title("🔥 APP IS WORKING")
+st.write("If you see this, problem is NOT Streamlit")
