3c05ba10ea1468a19fa887e5bca63670f8809fb5
sugunthanG <sugunthan100@gmail.com>
2026-04-05 13:40:13 +0530

    disable login

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 from app.login import login
 from dashboard.dashboard import run_dashboard
 
-# LOGIN
-if not login():
-    st.stop()
+# # LOGIN
+# if not login():
+#     st.stop()
 
 # DASHBOARD
 run_dashboard()
