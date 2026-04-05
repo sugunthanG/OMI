243b38fe25e79dfa86f2bde548905bbf84d70805
sugunthanG <sugunthan100@gmail.com>
2026-04-05 15:11:26 +0530

    final main fix

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import streamlit as st
 from app.login import login
 from dashboard.dashboard import run_dashboard
 
-# Session init
+# ---------------- SESSION INIT ----------------
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
-# LOGIN FLOW
+# ---------------- LOGIN FLOW ----------------
 if not st.session_state.logged_in:
     login()
     st.stop()
 
-# DASHBOARD
+# ---------------- DASHBOARD ----------------
 run_dashboard()
